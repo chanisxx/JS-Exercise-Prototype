2a09af5d21c28ac1bac6e7e9120ae2c8fca1613a
Chanis Torres <wonderland@Chaniss-MacBook-Pro.local>
2020-04-30 18:38:41 -0400

    Test 1 pass

--- index.js
+++ index.js
@@ -39,8 +39,21 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function (someFood) {
+  if (this.stomach.length <= 9) {
+    this.stomach.push(someFood);
+  }
+}
+Person.prototype.poop = function () {
+  this.stomach = [];
+}
+Person.prototype.toString = function () {
+  return `${this.name}, ${this.age}`;
 }
 
 /*
@@ -57,7 +70,7 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
 
 }
 
